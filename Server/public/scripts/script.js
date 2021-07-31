b8bd59d4a383cfe75dafac5370e4ea1d6a9231de
Curtis Kettler <curtiskettler@gmail.com>
2021-07-31 11:37:35 -0500

    computeMath function written on client side

--- Server/public/scripts/script.js
+++ Server/public/scripts/script.js
@@ -3,4 +3,29 @@ $(document).ready(onReady);
 
 function onReady() {
     console.log('JQ');
+    $('#equalsButton').on('click', computeMath);
+    $('#clearButton').on('click', clearCalc);
+    getHistory();
 };
+
+function computeMath(event){
+    console.log('in computeMath');
+    event.preventDefault();
+    let objectToSend = {
+        numOne: $('#firstNum').val(),
+        numTwo: $('#secondNum').val()
+    }
+    $.ajax({
+        type: 'POST',
+        url: '/equations',
+        data: objectToSend
+    }).then(function (response){
+        $('#firstNum').val(''),
+        $('#secondNum').val('')
+        getHistory();
+    }).catch(function (err){
+        alert('Something went wrong, please try again later.')
+        console.log(err);  
+    });
+    
+}
--- Server/server.js
+++ Server/server.js
@@ -1,11 +1,12 @@
 const express = require('express');
 const app = express();
+const bodyParser = require('body-parser');
 const port = 8080;
 app.use(express.static('./server/public'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: true}));
 
-
-
-
+const equations = [ ];
 
 
 
