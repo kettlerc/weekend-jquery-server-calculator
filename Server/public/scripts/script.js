3684e1c9a3cc8800508bd756cca77e97ca6a1412
Curtis Kettler <curtiskettler@gmail.com>
2021-08-02 09:39:14 -0500

    comments added

--- Server/public/scripts/script.js
+++ Server/public/scripts/script.js
@@ -1,29 +1,39 @@
 console.log('JS');
+//initializes JQuery
 $(document).ready(onReady);
+
 
 function onReady() {
     console.log('JQ');
+    //jquery to handle button clicks
     $('#equalsButton').on('click', computeMath);
     $('#clearButton').on('click', clearNums);
     $('#addButton').on('click', addNums);
     $('#subtractButton').on('click', subtractNums);
     $('#multiplyButton').on('click', multiplyNums);
     $('#divideButton').on('click', divideNums);
+
     keepHistory();
 };
 
+//empty object to hold data
 let objectToSend = {};
 
+//function to clear input fields
 function clearNums(){
     $('#firstNum').val(''),
     $('#secondNum').val('')
-};
+}; //end clearNums
 
+//function for posting data to server and getting result back
 function computeMath(event){
     console.log('in computeMath');
+    //prevents page from refreshing on for submit
     event.preventDefault();
+    //pushes input field data to empty object
     objectToSend['numOne'] = $('#firstNum').val();
     objectToSend['numTwo'] = $('#secondNum').val();
+    //AJAX to POST object to server
     $.ajax({
         type: 'POST',
         url: '/equations',
@@ -31,6 +41,8 @@ function computeMath(event){
     }).then(function (response){
         keepHistory();
     });
+    //AJAX to get result of equation back from server
+    //then append that data to the DOM
     $.ajax({
         type: 'GET',
         url: '/equations'
@@ -39,9 +51,12 @@ function computeMath(event){
         result.empty();
         result.append(`${response[response.length-1]}`);
     });
-};
+}; //end computeMath
 
+//function for getting equation history from server
 function keepHistory(){
+    //AJAX to GET history from server
+    //then loop through the data and append to DOM
     $.ajax({
         type: 'GET',
         url: '/history'
@@ -54,18 +69,18 @@ function keepHistory(){
             history.append(`<li>${thing}</li>`);
         }
     });
-};
+}; //end keepHistory
 
 //functions for catching operator data with buttons
 function addNums() {
     objectToSend['operator'] = '+';
-};
+}; //end addNums
 function subtractNums() {
     objectToSend['operator'] = '-';
-};
+}; //end subtract nums
 function multiplyNums() {
     objectToSend['operator'] = '*';
-};
+}; //end multiplyNums
 function divideNums() {
     objectToSend['operator'] = '/';
-};
+}; //end divideNums
--- Server/server.js
+++ Server/server.js
@@ -1,3 +1,4 @@
+//code to initialize express & body-parser
 const express = require('express');
 const app = express();
 const bodyParser = require('body-parser');
@@ -6,30 +7,37 @@ app.use(express.static('./server/public'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
 
+//empty objects that data is pushed into before
+//being sent back to client
 const equations = [];
 const answer = [];
 
+//POST receives initial data from client
 app.post('/equations', (req, res) => {
     console.log('in /equations post:', req.body);
     equations.push(req.body);
     res.sendStatus(201);
 });
 
+//runs doTheMath function on data and returns it to client
 app.get('/equations', (req, res) => {
     // console.log('in equations get', equations);
     doTheMath();
     res.send(answer);
 });
 
+//returns data to keepHistory function
 app.get('/history', (req, res) => {
     console.log('answer is', answer);
     res.send(answer);
 });
 
+//tells server to listen for requests
 app.listen(port, function(){
     console.log('listening on port:', port);
 });
 
+//function for computing math equations
 function doTheMath(){
     let numOne = Number(equations[equations.length-1].numOne);
     let numTwo = Number(equations[equations.length-1].numTwo);
@@ -55,4 +63,4 @@ function doTheMath(){
         alert('Please choose an operator!');
     }
     return answer;
-};
+}; //end doTheMath
